dc4f5ba9272d537c49429d0241627a3f5f8f464c
Litto Fernandez <124418872+LittoWinchester@users.noreply.github.com>
2023-03-07 16:28:21 -0300

    Delete function.js

--- function.js
+++
@@ -1,8 +0,0 @@
-console.log ("ESTA ANDANDO");
-
-document.getElementById("boton_certificado").addEventListener('click', function(){
-
-    document.getElementById("idcert").style="display:block"
-    
-
-});
