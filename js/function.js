e98df7931b9d20a674227d80906b71728e92d81e
Litto Fernandez <124418872+LittoWinchester@users.noreply.github.com>
2023-03-07 16:29:52 -0300

    Segunda vez que subo
    la primera no se cargaban las carpetas. hay que arrastrar todo junto

---
+++ js/function.js
@@ -0,0 +1,8 @@
+console.log ("ESTA ANDANDO");
+
+document.getElementById("boton_certificado").addEventListener('click', function(){
+
+    document.getElementById("idcert").style="display:block"
+    
+
+});
